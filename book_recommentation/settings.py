a25821d5b5d139ad4801d543fe60da474bc1915c
kuri-thevc <60633084+ku-ri@users.noreply.github.com>
2022-03-30 21:39:18 +0900

    final commit

--- book_recommentation/settings.py
+++ book_recommentation/settings.py
@@ -195,6 +195,9 @@ DEBUG_TOOLBAR_PANELS = [
 
 
 # static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+
 
 STATICFILES_FINDERS = ['django.contrib.staticfiles.finders.FileSystemFinder',]
